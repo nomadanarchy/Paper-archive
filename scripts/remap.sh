a2c945c9df3b60daf6ab7557b41ec89e6af33807
NomadAnarchy <284705814+nomadanarchy@users.noreply.github.com>
2026-09-21 13:02:58 -0500

    Apply local changes after rebase

--- scripts/remap.sh
+++ scripts/remap.sh
@@ -16,7 +16,8 @@ jarpath="$workdir/Minecraft/$minecraftversion/$minecraftversion"
 echo "Downloading unmapped vanilla jar..."
 if [ ! -f  "$jarpath.jar" ]; then
     mkdir -p "$workdir/Minecraft/$minecraftversion"
-    curl -s -o "$jarpath.jar" "https://s3.amazonaws.com/Minecraft.Download/versions/$minecraftversion/minecraft_server.$minecraftversion.jar"
+    serverurl=$(cat "${workdir}/BuildData/info.json" | grep serverUrl | cut -d '"' -f 4)
+    curl -s -o "$jarpath.jar" "$serverurl"
     if [ "$?" != "0" ]; then
         echo "Failed to download the vanilla server jar. Check connectivity or try again later."
         exit 1
